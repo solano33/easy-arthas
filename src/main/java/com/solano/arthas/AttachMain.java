729ba3198242a1715eb0521e557ff8358fb90d88
solano-h <solano33@163.com>
2024-08-19 00:42:07 +0800

    feat: easy-arthas

---
+++ src/main/java/com/solano/arthas/AttachMain.java
@@ -0,0 +1,65 @@
+package com.solano.arthas;
+
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * @author github.com/solano33
+ * @date 2024/8/19 00:27
+ */
+public class AttachMain {
+
+    public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
+        ProcessBuilder processBuilder = new ProcessBuilder().command("jps");
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        try {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } finally {
+            bufferedReader.close();
+        }
+        System.out.println(sb);
+
+        Scanner scanner = new Scanner(System.in);
+        String processId = scanner.nextLine();
+
+        VirtualMachine vm = VirtualMachine.attach(processId);
+        vm.loadAgent("/Users/solano/Documents/coding/github/java-enhance-tools/time-cost-agent/target/time-cost-agent-1.0-SNAPSHOT-jar-with-dependencies.jar");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/solano/arthas/command/MemoryCommand.java
@@ -0,0 +1,8 @@
+package com.solano.arthas.command;
+
+/**
+ * @author github.com/solano33
+ * @date 2024/8/19 00:27
+ */
+public class MemoryCommand {
+}
